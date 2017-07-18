47c307df513e1b5dcd58d4e0c6c34e69092bf0b2
mohytry <mohytry@aol.com>
2017-07-18 06:51:16 +0200

    somewhat functional build (some issue with crop)

--- ORA_LayerCropper/src/application/Main.java
+++ ORA_LayerCropper/src/application/Main.java
@@ -1,16 +1,18 @@
 package application;
 	
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipException;
 import java.util.zip.ZipFile;
+
+import javax.imageio.ImageIO;
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -18,13 +20,13 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.stage.Window;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
@@ -34,14 +36,21 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+//import org.apache.commons.io.FilenameUtils;
 
 
 public class Main extends Application {
+	
+	//layername, layer (k,v)
+	private Map<String, Image> layerMap;
+	
 	@Override
 	public void start(Stage stage) {
 		try {
@@ -50,7 +59,7 @@ public class Main extends Application {
 			
 			//root pane
 			BorderPane root = new BorderPane();
-			//test
+
 			//listview for layers
 			VBox vboxLeft = new VBox();
 			vboxLeft.setPadding(new Insets(10));
@@ -84,6 +93,7 @@ public class Main extends Application {
 			menuFile.getItems().add(menuFileOpen);
 			menuFileOpen.setAccelerator(KeyCombination.keyCombination("Ctrl+O"));
 			
+			//Open ORA-File
 			FileChooser fileChooser = new FileChooser();
 			menuFileOpen.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
@@ -95,9 +105,9 @@ public class Main extends Application {
 						//fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PNG", "*.png"));
 					File oraFile = fileChooser.showOpenDialog(stage);
 					
-					//obeservable for listView, map for imageView -> image ajustment on selection
+					//observable for listView, map for imageView -> image ajustment on selection
 					ObservableList<String> layerNames = FXCollections.observableArrayList();
-					Map<String, Image> layerMap = new HashMap<String, Image>();
+					layerMap = new HashMap<String, Image>();
 					
 					try {
 						ZipFile zip = new ZipFile(oraFile);
@@ -139,15 +149,39 @@ public class Main extends Application {
 				}
 			});
 			
-			//event handler for menuCrop
+			//Crop and Export Layers
 			DirectoryChooser dirChooser = new DirectoryChooser();
 			dirChooser.setTitle("Select Save-Folder for Layer Exports");
 			menuCrop.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent event) {
 					// TODO Auto-generated method stub
-					//TODO: crop all images to remove excess transparent pixels, open folder dialog for save location of layers
-					dirChooser.showDialog(stage);
+					
+					//select save folder for new cropped images
+					File selectedDir = dirChooser.showDialog(stage);
+					
+					int i=0;
+					//crop all images to remove excess transparent pixels
+					for(Entry<String, Image> entry : layerMap.entrySet())
+					{
+						Image img = entry.getValue();
+						String imgName = entry.getKey();
+						
+						//get cropped image
+						WritableImage wImg = CropImage(img);
+						//save images to selected folder
+						BufferedImage bImg = SwingFXUtils.fromFXImage(wImg, null);
+						try {
+							
+							String filename = selectedDir.getAbsolutePath()+"\\layer"+i+".png"; i++;
+							System.out.println(filename);
+							ImageIO.write(bImg, "png", new File(filename));
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					
 				}
 			});
 			
@@ -163,21 +197,29 @@ public class Main extends Application {
 			});
 			
 			
-			/*//file button
+			/*//test button
 			Button btn = new Button("test button bot");
 			//vboxLeft.getChildren().add(btn);
-			root.setBottom(btn);*/
-			
-			
+			root.setLeft(btn);
+			btn.setOnAction(new EventHandler<ActionEvent>() {
+				@Override
+				public void handle(ActionEvent event) {
+					// TODO Auto-generated method stub
+					//test writableimage to file
+					WritableImage wimg = new WritableImage(200, 200);
+					//wimg.getPixelWriter().setPixels(0, 0, 199, 199, Pixel, arg5, arg6);
+					BufferedImage bImg = SwingFXUtils.fromFXImage(wimg, null);
+					try {
+						ImageIO.write(bImg, "png", new File("testImg.png"));
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			});*/
 			
 			//create scene, add to stage
 			Scene scene = new Scene(root,750,400);
-		   /* stage.setMinWidth(400);
-		    stage.setMaxWidth(1200);
-		    stage.setMinHeight(200);
-			stage.setMaxHeight(800); 
-		    */
-		     
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			stage.setScene(scene);
 			stage.show();
@@ -187,40 +229,40 @@ public class Main extends Application {
 		}
 	}
 	
-	private ArrayList<Image> GetImagesFromORAFile(File file)
-	{
-		ArrayList<Image> imgList = new ArrayList<Image>();
-			
-		try {
-			ZipFile zip = new ZipFile(file);
-			Enumeration<? extends ZipEntry> entries = zip.entries();
-			while(entries.hasMoreElements())
-			{
-				ZipEntry entry = (ZipEntry)entries.nextElement();
-				//if png and not directory add file to list
-				//TODO: fix ugly code with FilenameUtils.getExtension(entry.getName()).equals("png") // requires apache commons
-				if(!entry.isDirectory() && entry.getName().endsWith(".png"))
-				{
-					imgList.add(InputStreamToImage(zip.getInputStream(entry)));
-					System.out.println("found png named: "+ entry.getName());
-				}
-			}
-			zip.close();
-			
-		} catch (ZipException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return imgList;
-	}
 	
 	private Image InputStreamToImage(InputStream is)
 	{
 		Image img = new Image(is);
 		return img;
+	}
+	
+	private WritableImage CropImage(Image img)
+	{
+		PixelReader pixelReader = img.getPixelReader();
+		//cropping values once adjusted, start comparing values to middle
+		int maxX = 0; int minX = 0;//(int)img.getWidth()-10; //problem with initial cropping settings?
+		int maxY = 0; int minY = 0;//(int)img.getHeight()-10; 
+		System.out.println("minY Start = "+ minY+ "minX start = " + minX);
+		double transparencyTolerance = 0.05;
+		
+		//read every row
+		for(int y = 0; y < img.getHeight(); y++)
+		{
+			for(int x = 0; x < img.getWidth(); x++)
+			{
+				//if not transparent, get new boudries for visible content
+				if(pixelReader.getColor(x, y).getOpacity() > transparencyTolerance)
+				{
+					minY = Math.min(minY, y);
+					maxY = Math.max(maxY, y);
+					minX = Math.min(minX, x);
+					maxX = Math.max(maxX, x);
+				}
+			}
+		}
+		//new cropped image
+		WritableImage newImg = new WritableImage(pixelReader, minX, minY, maxX, maxY);
+		return newImg;
 	}
 	
 	public static void main(String[] args) {
